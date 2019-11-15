6b710b3b91e66274bc8eaa566bede5712c1b805e
Nico Salvador <nico.salvador@bax-shop.nl>
2019-11-15 17:09:59 +0800

    Add smooth scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from "~/layouts/Default.vue"
+import VueScrollTo from "vue-scrollto";
+import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
 	// Set default layout as a global component
@@ -15,7 +16,9 @@ export default function(Vue, { router, head, isClient }) {
 		rel: "stylesheet",
 		href: "https://use.fontawesome.com/releases/v5.1.1/css/all.css",
 		integrity:
-			"sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ",
+		"sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ",
 		crossorigin: "anonymous"
 	})
+
+	Vue.use(VueScrollTo)
 }
